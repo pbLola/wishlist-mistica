16449427f02574a9cbcf405b5ae635a9f87fbd91
pbLola <lohannecastro88@gmail.com>
2026-05-27 17:35:56 -0300

    feat: logica de captura de dados e reset do formulario

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Sistema Iniciado!")
+const formulario = document.querySelector('#wishlist-form');
+const inputNome = document.querySelector('#input-nome');
+const inputPreco = document.querySelector('#input-preco');
+const inputLink = document.querySelector('#input-link');
+
+console.log(formulario);
+console.log(inputNome);
+console.log(inputPreco);
+console.log(inputLink);
+
+formulario.addEventListener('submit', function(evento){
+    evento.preventDefault();
+    const nomeDigitado = inputNome.value;
+    const precoDigitado = inputPreco.value;
+    const linkDigitado = inputPreco.value;
+
+    console.log("Nome do Produto:", nomeDigitado);
+    console.log("Preço do Produto:", precoDigitado);
+    console.log("Link da Shopee:", linkDigitado);
+
+    inputNome.value = '';
+    inputPreco.value = '';
+    inputLink.value = '';
+
+});
+
